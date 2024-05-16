f14c1a4d40ed83856282d46ec1458f6525802c2d
Amir Hossein Sheikholeslami <sheikholeslami@limoo.im>
2024-05-16 17:20:04 +0330

    Add test for invoice and play class

--- src/test/java/dramaplays/model/InvoiceTest.java
+++ src/test/java/dramaplays/model/InvoiceTest.java
@@ -2,11 +2,36 @@ package dramaplays.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class InvoiceTest {
 
-    @Test
-    void testInvoice() {
 
+    @Test
+    void testNullPerformance() {
+        assertThrows(Exception.class, () -> new Invoice("amirhossein", null));
+    }
+
+    @Test
+    void testNullCustomer() {
+        assertThrows(Exception.class, () -> new Invoice(null, Arrays.asList(
+                new Performance("0", 3),
+                new Performance("1", 4))));
+
+    }
+
+    @Test
+    void testValues() {
+        String customer = "amirhossein";
+        List<Performance> performanceList = Arrays.asList(new Performance("0", 3),
+                new Performance("1", 4));
+        Invoice invoice = new Invoice(customer, performanceList);
+        assertEquals(invoice.customer, customer);
+        assertEquals(invoice.performances, performanceList);
     }
 
 
--- src/test/java/dramaplays/model/PlayTest.java
+++ src/test/java/dramaplays/model/PlayTest.java
@@ -1,4 +1,29 @@
 package dramaplays.model;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class PlayTest {
+
+    @Test
+    void testNullName() {
+        assertThrows(Exception.class, () -> new Play(null, "1"));
+    }
+
+    @Test
+    void testNullType() {
+        assertThrows(Exception.class, () -> new Play("amirhossein", null));
+    }
+
+    @Test
+    void testValues() {
+        String name = "amirhossein";
+        String type = "0";
+        Play play = new Play(name, type);
+        assertEquals(play.name, name);
+        assertEquals(play.type, type);
+    }
+
 }
